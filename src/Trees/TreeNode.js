86253fb0a8befed5ab6f3c8efa14cff517313f25
Satyam <satyamgupta19946@gmail.com>
2020-12-22 16:54:03 +0530

    Initialized trees folder, need help with tree css.

---
+++ src/Trees/TreeNode.js
@@ -0,0 +1,10 @@
+class TreeNode {
+  constructor(value, leftChild, rightChild, parent) {
+    this.value = value;
+    this.leftChild = leftChild;
+    this.rightChild = rightChild;
+    this.parent = parent;
+  }
+}
+
+export default TreeNode;
